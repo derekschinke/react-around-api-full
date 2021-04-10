3afa34760f8d50ac6d288f9bcbc39323079da678
Derek Schinke <derekschinke@me.com>
2021-04-09 22:37:07 -0400

    fix: card schema

--- models/card.js
+++ models/card.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const schema = mongoose.Schema(
+const schema = new mongoose.Schema(
   {
     name: {
       type: String,
