3c1acf2924ac2228b82ab7f2ecd43ceec68392b4
Derek Schinke <derekschinke@me.com>
2021-04-16 23:05:51 -0400

    fix: change no-unused-vars

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ app.post(
 
 app.use(errors());
 
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
