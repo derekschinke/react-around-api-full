af6067de3d89885caadd81b3f74ac1d930ff9586
Derek Schinke <derekschinke@me.com>
2021-04-22 20:42:14 -0400

    upgrade: deep upgrade

--- .pnp.js
+++ .pnp.js
@@ -1528,7 +1528,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["semver", "npm:7.3.5"],
             ["strip-ansi", "npm:6.0.0"],
             ["strip-json-comments", "npm:3.1.1"],
-            ["table", "npm:6.3.2"],
+            ["table", "npm:6.3.3"],
             ["text-table", "npm:0.2.0"],
             ["v8-compile-cache", "npm:2.3.0"]
           ],
@@ -4703,14 +4703,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]
       ]],
       ["table", [
-        ["npm:6.3.2", {
-          "packageLocation": "./.yarn/cache/table-npm-6.3.2-4f4c276a65-8c016c3f92.zip/node_modules/table/",
+        ["npm:6.3.3", {
+          "packageLocation": "./.yarn/cache/table-npm-6.3.3-cbaedf7787-67d7c29c62.zip/node_modules/table/",
           "packageDependencies": [
-            ["table", "npm:6.3.2"],
+            ["table", "npm:6.3.3"],
             ["ajv", "npm:8.1.0"],
-            ["is-boolean-object", "npm:1.1.0"],
-            ["is-number-object", "npm:1.0.4"],
-            ["is-string", "npm:1.0.5"],
             ["lodash.clonedeep", "npm:4.5.0"],
             ["lodash.flatten", "npm:4.4.0"],
             ["lodash.truncate", "npm:4.4.2"],
